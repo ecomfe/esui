eb56ae8ea446d53d1b6dbfd4c077cff538bbb288
otakustay <otakustay@live.com>
2013-03-21 16:55:44 +0800

    Added first implement of ViewContext

---
+++ src/ViewContext.js
@@ -0,0 +1,92 @@
+/**
+ * ESUI (Enterprise Simple UI)
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file 视图环境类 用于对控件视图的管理
+ * @author DBear
+ */
+ define(
+    function() {
+        /**
+         * ViewContext类声明
+         *
+         * @constructor
+         */
+        function ViewContext() {
+            /**
+             * 视图环境控件集合
+             * @type {Object} 
+             */
+            this.controlMap = {};
+        }
+
+
+        /**
+         * 将控件实例添加到视图环境中
+         *
+         * @param {Control} control 待加控件
+         * @public
+         */
+        ViewContext.prototype.add = function (control) {
+            var exists = this.control[control.id];
+
+            // id已存在
+            if (exists) {
+                // 是同一控件，不做处理
+                if (exists === control) {
+                    return;
+                }
+
+                // 不是同一控件，先覆盖原关联控件的viewContext
+                exists.setViewContext(null);
+            }
+
+            this.controlMap[control.id] = control;
+            control.setViewContext(this);
+
+        };
+
+        /**
+         * 将控件实例从视图环境中移除。
+         *
+         * @param {Control} control 待移除控件
+         * @public
+         */
+        ViewContext.prototype.remove = function (control) {
+
+            this.controlMap[control.id] = null;
+            control.setViewContext(null);
+
+        };
+
+        /**
+         * 通过id获取控件实例。 
+         *
+         * @param {string} id 控件id
+         * @return {Control} 根据id获取的控件
+         * @public
+         */
+        ViewContext.prototype.get = function (id) {
+
+            return this.controlMap[id];
+
+        };
+
+        /**
+         * 清除视图环境中所有控件。 
+         *
+         */
+        ViewContext.prototype.clean = function () {
+            for (var id in this.controlMap) {
+                if (this.controlMap.hasOwnProperty(id)) {
+                    var control = this.controlMap[id];
+                    this.remove(control);
+                    control.dispose();
+                }
+            }
+        };
+
+        return ViewContext;
+
+    }
+);
