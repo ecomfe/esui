51361b937b977f5941579752544ec71ba94587e3
erik <erik168@163.com>
2013-03-22 11:04:33 +0800

    add Label Control

---
+++ src/Label.js
@@ -0,0 +1,22 @@
+/**
+ * ESUI (Enterprise Simple UI)
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file Label控件
+ * @author erik
+ */
+
+define(
+    function (require) {
+        var Control = require('./Control');
+        function Label(options) {
+            Control.call(this, options);
+        }
+
+        Label.prototype.type = "Label";
+
+        require('./lib').inherits(Label, Control);
+        require('./main').register(Label);
+        return Label;
+    }
+);
