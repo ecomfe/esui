694a198a39ef3124198f5b9e353ccb7517db4c90
otakustay <otakustay@live.com>
2013-04-08 14:53:48 +0800

    Added a Tab control implementation without allowClose property

--- src/Tab.js
+++ src/Tab.js
@@ -33,13 +33,16 @@ define(
          */
         Tab.prototype.initOptions = function (options) {
             var properties = {
-                tabs: []
+                tabs: [],
+                activeIndex: 0,
+                allowClose: false
             };
             var lib = require('./lib');
             lib.extend(properties, options);
 
             // 如果子元素中有一个`[data-role="navigator"]`的元素，
-            // 则应该从元素中去找出对应的标签页。
+            // 则应该从元素中去找出对应的标签页配置，然后这个元素就不要了，
+            // 控件会自动生成正确的`navigator`格式并放在`main`的最前面
             // 
             // 而如果有子元素且没有`[data-role="navigator"]`元素，
             // 则认为每个子元素是一个标签页，从`title`属性中找出对应的`title`
@@ -50,9 +53,7 @@ define(
                     if (element.getAttribute('data-role') === 'navigator') {
                         // 找到了`[data-role="navigator"]`的元素，抛弃其它配置
                         properties.tabs = [];
-                        // 同时认为`navigator`元素已经有了，
-                        // 不用在`initStructure`中再次创建
-                        properties.navigator = element;
+                        element.parentNode.removeChild(element);
                         for (var i = 0; i < element.children.length; i++) {
                             var tab = element.children[i];
                             var config = {
@@ -78,23 +79,56 @@ define(
             lib.extend(this, properties);
         };
 
+        /*
+         * 点击某个标签时的切换逻辑
+         *
+         * @param {Tab} tab Tab控件实例
+         * @param {Event} e 触发事件的事件对象
+         */
+        function selectTab(tab, e) {
+            var target = e.target;
+            while (target.nodeName.toLowerCase() !== 'li') {
+                target = target.parentNode;
+            }
+
+            if (target.nodeName.toLowerCase() === 'li') {
+                var parent = target.parentNode;
+                for (var i = 0; i < parent.children.length; i++) {
+                    if (parent.children[i] === target) {
+                        tab.activateAt(i);
+                    }
+                    return;
+                }
+            }
+        }
+
         /**
          * 初始化DOM结构
          *
          * @protected
          */
         Tab.prototype.initStructure = function () {
-            if (this.navigator) {
-                return;
-            }
-
             this.navigator = document.createElement('ul');
 
+            var lib = require('./lib');
+            this.main.insertBefore(this.navigator, this.main.firstChild);
+
+            require('./controlHelper').addDOMEvent(
+                this, this.navigator, 'click', lib.bind(selectTab, null, this));
+        };
+
+        /**
+         * 获取导航条的HTML
+         *
+         * @param {Array} tabs 标签页配置项
+         * @inner
+         */
+        function getNavigatorHTML(tabs) {
             var itemTemplate = '<li data-for="${panel}">${title}</li>';
             var html = '';
             var lib = require('./lib');
-            for (var i = 0; i < this.tabs.length; i++) {
-                var config = this.tabs[i];
+            for (var i = 0; i < tabs.length; i++) {
+                var config = tabs[i];
                 var data = {
                     panel: lib.encodeHTML(config.panel),
                     title: lib.encodeHTML(config.title)
@@ -102,8 +136,123 @@ define(
                 html += lib.format(itemTemplate, data);
             }
 
-            this.navigator.innerHTML = itemTemplate;
-            lib.insertBefore(this.navigator, this.main.firstChild);
+            return html;
+        }
+
+        /**
+         * 批量更新属性并重绘
+         *
+         * @param {Object} 需更新的属性
+         * @public
+         */
+        Tab.prototype.setProperties = function (properties) {
+            // 如果`tabs`配置变了，则会导致`navigator`整个重新渲染，
+            // 则后续的`repaint`处理`activeIndex`或`allowClose`会产生一次浪费，
+            // 因此这里直接把`activeIndex`放自己身上，等`navigator`渲染后使用，
+            // 不再把`activeIndex`和`allowClose`加入到`changes`集合中去
+            if (properties.tabs) {
+                if (properties.activeIndex != null) {
+                    this.activeIndex = properties.activeIndex;
+                    delete properties.activeIndex;
+                }
+                else {
+                    // 如果仅改变`tabs`，则由于标签页的数量变化，
+                    // 可能导致`activeIndex`不同步，需要同步一次
+                    // 
+                    // 仅在这里做分支，是因为如果用户传递`activeIndex`，
+                    // 应该由用户保证正确性，出错也能及时让开发者发现
+                    if (this.activeIndex >= properties.tabs.length) {
+                        this.activeIndex = 0;
+                    }
+                }
+
+                if (properties.allowClose != null) {
+                    this.allowClose = properties.allowClose;
+                    delete properties.allowClose;
+                }
+            }
+
+            Control.prototype.setProperties.apply(this, arguments);
+        };
+
+        /*
+         * 激活指定位置的标签页
+         *
+         * @param {Tab} tab Tab控件实例
+         * @parma {number} index 待激活的标签页的下标
+         */
+        function activateTab(tab, index) {
+            for (var i = 0; i < tab.tabs.length; i++) {
+                var config = tab.tabs[i];
+                var lib = require('./lib');
+
+                if (config.panel) {
+                    var panel = lib.g(config.panel);
+                    if (panel) {
+                        panel.style.display = i === index ? '' : 'none';
+                    }
+                }
+
+                var tabElement = tab.navigator.children[i];
+                var methodName = i === index ? 'addClass' : 'removeClass';
+                lib[methodName](tabElement, 'ui-tab-active');
+            }
+        }
+
+        // 默认情况下只要处理了`tabs`就啥都处理完了
+        var allProperties = [
+            { name: 'tabs' }
+        ];
+
+        /**
+         * 重绘
+         *
+         * @param {Array=} 更新过的属性集合
+         * @protected
+         */
+        Tab.prototype.repaint = function (changes) {
+            changes = changes || allProperties;
+
+            for (var i = 0; i < changes.length; i++) {
+                var record = changes[i];
+                if (record.name === 'tabs') {
+                    this.navigator.innerHTML = getNavigatorHTML(this.tabs);
+                    // `tabs`变化导致`navigator`重新渲染，
+                    // 同时`activeIndex`的变化会被`setProperties`吞掉，
+                    // 因此要同步一次
+                    activateTab(this, this.activeIndex);
+                }
+                else if (record.name === 'activeIndex') {
+                    activateTab(this, this.activeIndex);
+                }
+                else if (record.name === 'allowClose') {
+                    // TODO: 实现allowClose
+                }
+            }
+        };
+
+        /**
+         * 激活一个标签页
+         *
+         * @param {Object} config 标签页的配置对象
+         * @public
+         */
+        Tab.prototype.activate = function (config) {
+            for (var i = 0; i < this.tabs.length; i++) {
+                if (this.tabs[i] === config) {
+                    this.activateAt(i);
+                }
+            }
+        };
+
+        /**
+         * 激活指定位置的标签页
+         *
+         * @param {number} index 待激活的标签页的下标
+         * @public
+         */
+        Tab.prototype.activateAt = function (index) {
+            this.setProperties({ activeIndex: index });
         };
 
         /**
@@ -114,7 +263,40 @@ define(
          * @param {string=} config.panel 标签页对应的容器的id
          */
         Tab.prototype.add = function (config) {
-            // TODO: 实现之
+            this.insert(config, this.tabs.length);
+        };
+
+        /**
+         * 在指定位置添加一个标签页
+         *
+         * @param {Object} config 标签页的配置
+         * @param {string} config.title 标签页的标题
+         * @param {string=} config.panel 标签页对应的容器的id
+         */
+        Tab.prototype.insert = function (config, index) {
+            this.tabs.splice(index, 0, config);
+            // 因为可以从已有的HTML中找出`navigator`，
+            // 所以鬼知道用户用的是`ul`还是`div`，
+            // 为了避免破坏内容模型，这里进行一下容器元素的判断
+            var tabElement = document.createElement('li');
+            var lib = require('./lib');
+            tabElement.innerHTML = lib.encodeHTML(config.title);
+            this.navigator.insertBefore(
+                tabElement, this.navigator.children[index]);
+
+            // 如果在当前激活的标签前插入一个，则`activeIndex`需要变化，
+            // 但视图是不用刷新的
+            if (index <= this.activeIndex) {
+                this.activeIndex++;
+            }
+
+            // 新加入的标签默认要隐藏起来
+            if (config.panel) {
+                var panel = lib.g(config.panel);
+                if (panel) {
+                    panel.style.display = 'none';
+                }
+            }
         };
 
         /**
@@ -123,7 +305,11 @@ define(
          * @param {Object} config 标签页的配置
          */
         Tab.prototype.remove = function (config) {
-            // TODO: 实现之
+            for (var i = 0; i < this.tabs.length; i++) {
+                if (this.tabs[i] === config) {
+                    this.removeAt(i);
+                }
+            }
         };
 
         /**
@@ -132,7 +318,29 @@ define(
          * @param {number} index 需要移除的标签页的下标
          */
         Tab.prototype.removeAt = function (index) {
-            // TODO: 实现之
+            var removed = this.tabs.splice(index, 1);
+            if (removed.length) {
+                var tabElement = this.navigator.children[index];
+                tabElement.parentNode.removeChild(tabElement);
+
+                // 如果删的标签在当前激活的标签的前面，
+                // 则当前激活的标签的下标其实改变了，`activeIndex`是要调整的，
+                // 但这种情况下实际激活的还是同一个标签，不用重新渲染
+                if (index < this.activeIndex) {
+                    this.activeIndex--;
+                }
+
+                // 如果正好激活的标签被删了，则把激活标签换成当前的后一个，
+                // 如果没有后一个了，则换成最后一个，这需要重新渲染
+                if (index === this.activeIndex) {
+                    // 由于可能`activeIndex`没变，因此不能走`setProperties`流程
+                    this.activeIndex = Math.min(
+                        this.activeIndex, 
+                        this.tabs.length - 1
+                    );
+                    activateTab(this, this.activeIndex);
+                }
+            }
         };
 
         // TODO: 添加`allowClose`属性的控制
--- test/Tab.js
+++ test/Tab.js
@@ -1,5 +1,11 @@
 define(function (require) {
     var Tab = require('esui/Tab');
+    var container = document.getElementById('container');
+    var tabs = [
+        { title: 'tab1', panel: 'a' },
+        { title: 'tab2', panel: 'b' },
+        { title: 'tab3', panel: 'c' }
+    ];
 
     describe('Tab', function () {
         it('should be a constructor', function () {
@@ -27,11 +33,6 @@ define(function (require) {
                 ];
                 main.innerHTML = html.join('\n');
                 var tab = new Tab({ main: main });
-                var tabs = [
-                    { title: 'tab1', panel: 'a' },
-                    { title: 'tab2', panel: 'b' },
-                    { title: 'tab3', panel: 'c' }
-                ];
                 expect(tab.get('tabs')).toEqual(tabs);
             });
 
@@ -82,6 +83,195 @@ define(function (require) {
                     { title: 'tab1', panel: 'a' }
                 ];
                 expect(tab.get('tabs')).toEqual(tabs);
+            });
+        });
+
+        describe('generally', function () {
+            it('should create a navigator `<ul>` element', function () {
+                var tab = new Tab({ tabs: tabs });
+                tab.appendTo(container);
+                var navigator = container.getElementsByTagName('ul')[0];
+                expect(navigator).toBeDefined();
+                expect(navigator.children.length).toBe(3);
+                expect(navigator.children[0].innerHTML).toBe('tab1');
+                expect(navigator.children[1].innerHTML).toBe('tab2');
+                expect(navigator.children[2].innerHTML).toBe('tab3');
+            });
+
+            it('should rerender navigator `<ul>` element if `tabs` is changed via `setProperties`', function () {
+                var tab = new Tab({ tabs: tabs });
+                tab.appendTo(container);
+                tab.setProperties({
+                    tabs: [
+                        { title: 'tab4', panel: 'd' }
+                    ]
+                });
+                var navigator = container.getElementsByTagName('ul')[0];
+                expect(navigator).toBeDefined();
+                expect(navigator.children.length).toBe(1);
+                expect(navigator.children[0].innerHTML).toBe('tab4');
+            });
+
+            it('should reset `activeIndex` to 0 if `tabs` is changed via `setProperties` and current `activeIndex` is out of range', function () {
+                var tab = new Tab({ tabs: tabs, activeIndex: 2 });
+                tab.appendTo(container);
+                tab.setProperties({
+                    tabs: [
+                        { title: 'tab4', panel: 'd' }
+                    ]
+                });
+                expect(tab.get('activeIndex')).toBe(0);
+            });
+
+            it('should add a `ui-tab-active` class for selected tab', function () {
+                var tab = new Tab({ tabs: tabs, activeIndex: 1 });
+                tab.appendTo(container);
+                var tabElement = container.getElementsByTagName('li')[1];
+                expect(tabElement.className).toMatch(/ui-tab-active/);
+            });
+
+            it('should change the `display` style of all panel when tab is rendered', function () {
+                var html = [
+                    '<div id="a"></div>',
+                    '<div id="b"></div>',
+                    '<div id="c"></div>'
+                ];
+                container.innerHTML = html.join('\n');
+                var tab = new Tab({ tabs: tabs, activeIndex: 1 });
+                tab.appendTo(container);
+                expect(document.getElementById('a').style.display).toBe('none');
+                expect(document.getElementById('b').style.display).toBe('');
+                expect(document.getElementById('c').style.display).toBe('none');
+            });
+
+            it('should hide all panel if a tab without `panel` property is activated', function () {
+                var html = [
+                    '<div id="a"></div>',
+                    '<div id="b"></div>'
+                ];
+                container.innerHTML = html.join('\n');
+                var tab = new Tab({
+                    tabs: [
+                        { title: 'tab1', panel: 'a' },
+                        { title: 'tab2', panel: 'b' },
+                        { title: 'tab3' },
+                    ], 
+                    activeIndex: 2
+                });
+                tab.appendTo(container);
+                expect(document.getElementById('a').style.display).toBe('none');
+                expect(document.getElementById('b').style.display).toBe('none');
+            });
+
+            it('should correctly adjust tab element and relative panel when `activate` is called', function () {
+                var html = [
+                    '<div id="a"></div>',
+                    '<div id="b"></div>',
+                    '<div id="c"></div>'
+                ];
+                container.innerHTML = html.join('\n');
+                var tab = new Tab({ tabs: tabs, activeIndex: 1 });
+                tab.appendTo(container);
+                tab.activate(tabs[2]);
+                expect(tab.get('activeIndex')).toBe(2);
+                expect(document.getElementById('a').style.display).toBe('none');
+                expect(document.getElementById('b').style.display).toBe('none');
+                expect(document.getElementById('c').style.display).toBe('');
+            });
+
+            it('should correctly adjust tab element and relative panel when `activateAt` is called', function () {
+                var html = [
+                    '<div id="a"></div>',
+                    '<div id="b"></div>',
+                    '<div id="c"></div>'
+                ];
+                container.innerHTML = html.join('\n');
+                var tab = new Tab({ tabs: tabs, activeIndex: 1 });
+                tab.appendTo(container);
+                tab.activateAt(2);
+                expect(tab.get('activeIndex')).toBe(2);
+                expect(document.getElementById('a').style.display).toBe('none');
+                expect(document.getElementById('b').style.display).toBe('none');
+                expect(document.getElementById('c').style.display).toBe('');
+            });
+
+            it('should add a tab config & element at the tail when `add` is called', function () {
+                var tab = new Tab({ tabs: tabs.slice() });
+                tab.appendTo(container);
+                var newTab = { title: 'tab4', panel: 'd' };
+                tab.add(newTab);
+                expect(tab.get('tabs')[3]).toEqual(newTab);
+                var navigator = container.getElementsByTagName('ul')[0];
+                expect(navigator.children.length).toBe(4);
+                expect(navigator.children[3].innerHTML).toBe('tab4');
+            });
+
+            it('should add a tab config & element at the given position when `insert` is called', function () {
+                var tab = new Tab({ tabs: tabs.slice() });
+                tab.appendTo(container);
+                var newTab = { title: 'tab4', panel: 'd' };
+                tab.insert(newTab, 2);
+                expect(tab.get('tabs')[2]).toEqual(newTab);
+                var navigator = container.getElementsByTagName('ul')[0];
+                expect(navigator.children.length).toBe(4);
+                expect(navigator.children[2].innerHTML).toBe('tab4');
+            });
+
+            it('should change `activeIndex` correctly when a tab is inserted', function () {
+                var tab = new Tab({ tabs: tabs.slice(), activeIndex: 1 });
+                tab.appendTo(container);
+                tab.insert({ title: 'tab4', panel: 'd' }, 0);
+                expect(tab.get('activeIndex')).toBe(2);
+            });
+
+            it('should hide a newly added tab\'s target panel', function () {
+                container.innerHTML = '<div id="d"></div>';
+                var tab = new Tab({ tabs: tabs.slice(), activeIndex: 1 });
+                tab.appendTo(container);
+                tab.add({ title: 'tab4', panel: 'd' });
+                expect(document.getElementById('d').style.display).toBe('none');
+            });
+
+            it('should remove a tab config & element when `remove` is called', function () {
+                var tab = new Tab({ tabs: tabs.slice() });
+                tab.appendTo(container);
+                tab.remove(tab.get('tabs')[1]);
+                expect(tab.get('tabs').length).toBe(2);
+                expect(tab.get('tabs')[0]).toEqual({ title: 'tab1', panel: 'a' });
+                expect(tab.get('tabs')[1]).toEqual({ title: 'tab3', panel: 'c' });
+                var navigator = container.getElementsByTagName('ul')[0];
+                expect(navigator.children.length).toBe(2);
+                expect(navigator.children[0].innerHTML).toBe('tab1');
+                expect(navigator.children[1].innerHTML).toBe('tab3');
+            });
+
+            it('should remove a tab config & element when `removeAt` is called', function () {
+                var tab = new Tab({ tabs: tabs.slice() });
+                tab.appendTo(container);
+                tab.removeAt(1);
+                expect(tab.get('tabs').length).toBe(2);
+                expect(tab.get('tabs')[0]).toEqual({ title: 'tab1', panel: 'a' });
+                expect(tab.get('tabs')[1]).toEqual({ title: 'tab3', panel: 'c' });
+                var navigator = container.getElementsByTagName('ul')[0];
+                expect(navigator.children.length).toBe(2);
+                expect(navigator.children[0].innerHTML).toBe('tab1');
+                expect(navigator.children[1].innerHTML).toBe('tab3');
+            });
+
+            it('should reset `activeIndex` to the next one if the active tab is removed', function () {
+                var tab = new Tab({ tabs: tabs.slice(), activeIndex: 1 });
+                tab.appendTo(container);
+                tab.removeAt(1);
+                expect(tab.get('activeIndex')).toBe(1);
+                expect(container.getElementsByTagName('li')[1].className).toMatch(/ui-tab-active/);
+            });
+
+            it('should set active tab to the last one if the last tab is active and removed', function () {
+                var tab = new Tab({ tabs: tabs.slice(), activeIndex: 2 });
+                tab.appendTo(container);
+                tab.removeAt(2);
+                expect(tab.get('activeIndex')).toBe(1);
+                expect(container.getElementsByTagName('li')[1].className).toMatch(/ui-tab-active/);
             });
         });
     });
