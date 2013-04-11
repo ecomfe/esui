fa0c3287f576e6b138fa17f7a601029f8426f28a
otakustay <otakustay@live.com>
2013-04-11 17:50:05 +0800

    Modified to made Layer a set of helper functions rather than a Control

--- src/controlHelper.js
+++ src/controlHelper.js
@@ -602,6 +602,212 @@ define(
             };
         };
 
+        var layer = helper.layer = {};
+        var zIndexStack = 1000;
+
+        // 统一方法
+        function render(element, options) {
+            var properties = require('./lib').clone(options || {});
+
+            // 如果同时有`top`和`bottom`，则计算出`height`来
+            if (properties.hasOwnProperty('top')
+                && properties.hasOwnProperty('bottom')
+            ) {
+                properties.height = properties.bottom - properties.top;
+                delete properties.bottom;
+            }
+            // 同样处理`left`和`right`
+            if (properties.hasOwnProperty('left')
+                && properties.hasOwnProperty('right')
+            ) {
+                properties.width = properties.right - properties.left;
+                delete properties.right;
+            }
+
+            // 避免原来的属性影响
+            if (properties.hasOwnProperty('top')
+                || properties.hasOwnProperty('bottom')
+            ) {
+                element.style.top = '';
+                element.style.bottom = '';
+            }
+
+            if (properties.hasOwnProperty('left')
+                || properties.hasOwnProperty('right')
+            ) {
+                element.style.left = '';
+                element.style.right = '';
+            }
+
+            // 设置位置和大小
+            for (var name in properties) {
+                if (properties.hasOwnProperty(name)) {
+                    element.style[name] = properties[name] + 'px';
+                }
+            }
+        }
+
+        /**
+         * 创建层元素
+         *
+         * @param {string=} tagName 元素的标签名，默认为`div`
+         * @return {HTMLElement}
+         */
+        layer.create = function (tagName) {
+            var element = document.createElement(tagName || 'div');
+            element.style.position = 'absolute';
+            return element;
+        };
+
+        /**
+         * 将当前层移到最前
+         *
+         * @param {HTMLElement} element 目标层元素
+         * @public
+         */
+        layer.moveToTop = function (element) {
+            element.style.zIndex = ++zIndexStack;
+        };
+
+        /**
+         * 移动层的位置
+         *
+         * @param {HTMLElement} element 目标层元素
+         * @param {number} top 上边界
+         * @param {number} left 左边界
+         * @public
+         */
+        layer.moveTo = function (element, top, left) {
+            render(element, { top: top, left: left });
+        };
+
+        /**
+         * 缩放层的大小
+         *
+         * @param {HTMLElement} element 目标层元素
+         * @param {number} width 宽度
+         * @param {number} height 高度
+         * @public
+         */
+        layer.resize = function (element, width, height) {
+            render(element, { width: width, height: height });
+        };
+
+        /**
+         * 让当前层靠住一个元素
+         *
+         * @param {HTMLElement} element 目标层元素
+         * @param {HTMLElement} target 目标元素
+         * @param {Object=} options 停靠相关的选项
+         * @param {string=} options.top 指示当前层的上边缘靠住元素的哪个边，
+         * 可选值为**top**或**bottom**
+         * @param {string=} options.bottom 指示当前层的下边缘靠住元素的哪个边，
+         * 可选值为**top**或**bottom**，* 当`top`值为**bottom**时，该值无效
+         * @param {string=} options.left 指示当前层的左边缘靠住元素的哪个边，
+         * 可选值为**left**或**right**
+         * @param {string=} options.right 指示当前层的下边缘靠住元素的哪个边，
+         * 可选值为**left**或**right**，* 当`left`值为**right**时，该值无效
+         * @param {number=} options.width 指定层的宽度
+         * @param {number=} options.height 指定层的高度
+         * @public
+         */
+        layer.attachTo = function (element, target, options) {
+            options = options || { left: 'left', top: 'top' };
+            var lib = require('./lib');
+            var offset = lib.getOffset(target);
+            // 有2种特殊的情况：
+            // 
+            // -`{ top: 'top', bottom: 'bottom' }`
+            // -`{ left: 'left', right: 'right' }`
+            // 
+            // 这两种情况下，要计算出宽和高来，且覆盖掉提供的宽高
+            var config = lib.clone(options);
+            if (config.top === 'top' && config.bottom === 'bottom') {
+                config.height = offset.height;
+                config.bottom = null;
+            }
+            if (config.left === 'left' && config.right === 'right') {
+                config.width = offset.width;
+                config.right = null;
+            }
+
+            var properties = {};
+            if (config.width) {
+                properties.width = config.width;
+            }
+            if (config.height) {
+                properties.height = config.height;
+            }
+
+            if (config.left) {
+                properties.left = offset[config.left];
+            }
+            else if (config.right) {
+                properties.right = offset[config.right];
+            }
+
+            if (config.top) {
+                properties.top = offset[config.top];
+            }
+            else if (config.bottom) {
+                properties.bottom = offset[config.bottom];
+            }
+
+            render(element, properties);
+        };
+
+        /**
+         * 将层在视图中居中
+         *
+         * @param {HTMLElement} element 目标层元素
+         * @param {Object=} options 相关配置项
+         * @param {number=} options.width 指定层的宽度
+         * @param {number=} options.height 指定层的高度
+         * @param {number=} options.minTop 如果层高度超过视图高度，
+         * 则留下该值的上边界保底
+         * @param {number=} options.minLeft 如果层宽度超过视图高度，
+         * 则留下该值的左边界保底
+         * @public
+         */
+        layer.centerToView = function (element, options) {
+            var lib = require('./lib');
+            var properties = options ? lib.clone(options) : {};
+
+            if (typeof properties.width !== 'number') {
+                properties.width = this.width;
+            }
+            if (typeof properties.height !== 'number') {
+                properties.height = this.height;
+            }
+
+            properties.left = (lib.page.getViewWidth() - properties.width) / 2;
+
+            var viewHeight = lib.page.getViewHeight();
+            if (properties.height >= viewHeight && 
+                options.hasOwnProperty('minTop')
+            ) {
+                properties.top = options.minTop;
+            }
+            else {
+                properties.top = 
+                    Math.floor((viewHeight - properties.height) / 2);
+            }
+
+            var viewWidth = lib.page.getViewWidth();
+            if (properties.height >= viewWidth && 
+                options.hasOwnProperty('minLeft')
+            ) {
+                properties.left = options.minLeft;
+            }
+            else {
+                properties.left = 
+                    Math.floor((viewWidth - properties.width) / 2);
+            }
+
+            properties.top += lib.page.getScrollTop();
+            this.setProperties(properties);
+        };
+
         return helper;
     }
 );
