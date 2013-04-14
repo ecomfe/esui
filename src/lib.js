aa6c38fed5d506ec527f4f39eb29628947fa90ec
otakustay <otakustay@live.com>
2013-04-14 23:03:20 +0800

    Simplified g method

--- src/lib.js
+++ src/lib.js
@@ -33,7 +33,7 @@ define(function () {
      * @param {object} source
      * @param {boolean}
      */
-    lib.isArray = function( source ){
+    lib.isArray = function (source) {
         return '[object Array]' == Object.prototype.toString.call(source);
     };
 
@@ -84,33 +84,36 @@ define(function () {
         return target;
     };
 
-    lib.isPlain  = function(obj){
-        var hasOwnProperty = Object.prototype.hasOwnProperty,
-            key;
-        if ( !obj ||
-             //一般的情况，直接用toString判断
-             Object.prototype.toString.call(obj) !== "[object Object]" ||
-             //IE下，window/document/document.body/HTMLElement/HTMLCollection/NodeList等DOM对象上一个语句为true
-             //isPrototypeOf挂在Object.prototype上的，因此所有的字面量都应该会有这个属性
-             //对于在window上挂了isPrototypeOf属性的情况，直接忽略不考虑
-             !('isPrototypeOf' in obj)
-           ) {
+    lib.isPlain  = function (obj) {
+        var hasOwnProperty = Object.prototype.hasOwnProperty;
+        if (!obj
+            // 一般的情况，直接用toString判断
+            || Object.prototype.toString.call(obj) !== '[object Object]'
+            // IE下，DOM和BOM对象上一个语句为true，
+            // isPrototypeOf挂在Object.prototype上的，
+            // 因此所有的字面量都应该会有这个属性
+            // 对于在window上挂了isPrototypeOf属性的情况，直接忽略不考虑
+            || !('isPrototypeOf' in obj)
+        ) {
             return false;
         }
 
-        //判断new fun()自定义对象的情况
-        //constructor不是继承自原型链的
-        //并且原型中有isPrototypeOf方法才是Object
-        if ( obj.constructor &&
-            !hasOwnProperty.call(obj, "constructor") &&
-            !hasOwnProperty.call(obj.constructor.prototype, "isPrototypeOf") ) {
+        // 判断new fun()自定义对象的情况
+        // constructor不是继承自原型链的
+        // 并且原型中有isPrototypeOf方法才是Object
+        if (obj.constructor
+            && !hasOwnProperty.call(obj, 'constructor')
+            && !hasOwnProperty.call(obj.constructor.prototype, 'isPrototypeOf')
+        ) {
             return false;
         }
-        //判断有继承的情况
-        //如果有一项是继承过来的，那么一定不是字面量Object
-        //OwnProperty会首先被遍历，为了加速遍历过程，直接看最后一项
-        for ( key in obj ) {}
-        return key === undefined || hasOwnProperty.call( obj, key );
+        // 判断有继承的情况
+        // 如果有一项是继承过来的，那么一定不是字面量Object
+        // OwnProperty会首先被遍历，为了加速遍历过程，直接看最后一项
+        var key;
+        for (key in obj) {
+        }
+        return key === undefined || hasOwnProperty.call(obj, key);
     };
 
 
@@ -125,15 +128,18 @@ define(function () {
         if (!source
             || source instanceof Number
             || source instanceof String
-            || source instanceof Boolean) {
+            || source instanceof Boolean
+        ) {
             return result;
-        } else if ( lib.isArray(source) ) {
+        }
+        else if (lib.isArray(source)) {
             result = [];
             var resultLen = 0;
             for (i = 0, len = source.length; i < len; i++) {
                 result[resultLen++] = lib.clone(source[i]);
             }
-        } else if (lib.isPlain(source)) {
+        }
+        else if (lib.isPlain(source)) {
             result = {};
             for (i in source) {
                 if (source.hasOwnProperty(i)) {
@@ -187,18 +193,11 @@ define(function () {
      * @return {HTMLElement|null} 获取的元素，查找不到时返回null
      */
     lib.g = function (id) {
-        // by Tangram 1.x: baidu.dom.g
         if (!id) {
             return null;
         }
 
-        if (typeof id === 'string' || id instanceof String) {
-            return document.getElementById(id);
-        }
-        else if (id.nodeName && (id.nodeType === 1 || id.nodeType === 9)) {
-            return id;
-        }
-        return null;
+        return typeof id === 'string' ? document.getElementById(id) : id;
     };
 
     /**
@@ -465,11 +464,13 @@ define(function () {
     };
 
 
-    lib.getComputedStyle = function(element, key){
-        var doc = element.nodeType == 9 ? element : element.ownerDocument || element.document;
-        var styles;
+    lib.getComputedStyle = function (element, key) {
+        var doc = element.nodeType == 9 
+            ? element 
+            : element.ownerDocument || element.document;
+
         if (doc.defaultView && doc.defaultView.getComputedStyle) {
-            styles = doc.defaultView.getComputedStyle(element, null);
+            var styles = doc.defaultView.getComputedStyle(element, null);
             if (styles) {
                 return styles[key] || styles.getPropertyValue(key);
             }
@@ -520,7 +521,7 @@ define(function () {
     lib.camelize = function (source) {
         return source.replace( 
             /-([a-z])/g, 
-            function ( alpha ) {
+            function (alpha) {
                 return alpha.toUpperCase();
             }
         );
@@ -610,9 +611,10 @@ define(function () {
      *
      * @return {number} 横向滚动量
      */
-    lib.page.getScrollLeft = function(){
+    lib.page.getScrollLeft = function () {
         return window.pageXOffset 
-            || document.documentElement.scrollLeft || document.body.scrollLeft;
+            || document.documentElement.scrollLeft
+            || document.body.scrollLeft;
     };
 
 
@@ -624,11 +626,12 @@ define(function () {
      * @param event 事件对象
      * @return void
      */
-    lib.event.preventDefault  = function( event ){
+    lib.event.preventDefault  = function (event) {
         if (event.preventDefault) {
-           event.preventDefault();
-        } else {
-           event.returnValue = false;
+            event.preventDefault();
+        }
+        else {
+            event.returnValue = false;
         }
     };
 
@@ -638,11 +641,12 @@ define(function () {
      * @return void
      */
     lib.event.stopPropagation = function (event) {
-       if (event.stopPropagation) {
-           event.stopPropagation();
-       } else {
-           event.cancelBubble = true;
-       }
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        }
+        else {
+            event.cancelBubble = true;
+        }
     };
 
     /**
@@ -713,16 +717,15 @@ define(function () {
      * 
      * @return {string}
      */
-    lib.getCommandStr = function( source ){
-        var dataCommandPrefix   = 'data-command-';
+    lib.getCommandStr = function (source) {
+        var dataCommandPrefix = 'data-command-';
         var result = [];
 
-        for ( var i in source ) {
-            if( source.hasOwnProperty(i) ){
-                var item = source[i];
-                result.push( dataCommandPrefix + i + '="' + source[i] + '"')
+        for (var i in source) {
+            if (source.hasOwnProperty(i)) {
+                result.push(dataCommandPrefix + i + '="' + source[i] + '"');
             }
-        };
+        }
         return result.join(' ');
     };
 
@@ -734,22 +737,22 @@ define(function () {
      * @param {object} element dom元素对象（可选）
      * @return {object}
      */
-    lib.getCommandAttr = function( source, element ){
-        var dataCommandPrefix   = 'data-command-';
+    lib.getCommandAttr = function (source, element) {
+        var dataCommandPrefix = 'data-command-';
         var result = {};
 
-        for ( var i in source ){
-            if( source.hasOwnProperty(i) ){
+        for (var i in source) {
+            if (source.hasOwnProperty(i)) {
                 var attrName  = dataCommandPrefix + i;
                 var attrValue = source[i];
 
-                result[ attrName ] = attrValue ;
+                result[attrName] = attrValue;
                 
-                if( element ){
-                    element.setAttribute( attrName, attrValue );
+                if (element) {
+                    element.setAttribute(attrName, attrValue);
                 }
             }
-        };
+        }
         return result;
     };
 
@@ -830,40 +833,52 @@ define(function () {
      * @param {string} source 目标字符串
      * @remark
      * 
-    对于目标字符串，下面这些规则决定了 parse 方法能够成功地解析： <br>
-    <ol>
-    <li>短日期可以使用“/”或“-”作为日期分隔符，但是必须用月/日/年的格式来表示，例如"7/20/96"。</li>
-    <li>以 "July 10 1995" 形式表示的长日期中的年、月、日可以按任何顺序排列，年份值可以用 2 位数字表示也可以用 4 位数字表示。如果使用 2 位数字来表示年份，那么该年份必须大于或等于 70。 </li>
-    <li>括号中的任何文本都被视为注释。这些括号可以嵌套使用。 </li>
-    <li>逗号和空格被视为分隔符。允许使用多个分隔符。 </li>
-    <li>月和日的名称必须具有两个或两个以上的字符。如果两个字符所组成的名称不是独一无二的，那么该名称就被解析成最后一个符合条件的月或日。例如，"Ju" 被解释为七月而不是六月。 </li>
-    <li>在所提供的日期中，如果所指定的星期几的值与按照该日期中剩余部分所确定的星期几的值不符合，那么该指定值就会被忽略。例如，尽管 1996 年 11 月 9 日实际上是星期五，"Tuesday November 9 1996" 也还是可以被接受并进行解析的。但是结果 date 对象中包含的是 "Friday November 9 1996"。 </li>
-    <li>JScript 处理所有的标准时区，以及全球标准时间 (UTC) 和格林威治标准时间 (GMT)。</li> 
-    <li>小时、分钟、和秒钟之间用冒号分隔，尽管不是这三项都需要指明。"10:"、"10:11"、和 "10:11:12" 都是有效的。 </li>
-    <li>如果使用 24 小时计时的时钟，那么为中午 12 点之后的时间指定 "PM" 是错误的。例如 "23:15 PM" 就是错误的。</li> 
-    <li>包含无效日期的字符串是错误的。例如，一个包含有两个年份或两个月份的字符串就是错误的。</li>
-    </ol>
-            
+     * 对于目标字符串，下面这些规则决定了 parse 方法能够成功地解析：
+     * 
+     * - 短日期可以使用“/”或“-”作为日期分隔符，
+     * 但是必须用月/日/年的格式来表示，例如"7/20/96"。
+     * - 以 "July 10 1995" 形式表示的长日期中的年、月、日可以按任何顺序排列，
+     * 年份值可以用 2 位数字表示也可以用 4 位数字表示。
+     * 如果使用 2 位数字来表示年份，那么该年份必须大于或等于 70。
+     * - 括号中的任何文本都被视为注释。这些括号可以嵌套使用。
+     * - 逗号和空格被视为分隔符。允许使用多个分隔符。
+     * - 月和日的名称必须具有两个或两个以上的字符。
+     * 如果两个字符所组成的名称不是独一无二的，
+     * 那么该名称就被解析成最后一个符合条件的月或日。
+     * 例如，"Ju" 被解释为七月而不是六月。
+     * - 在所提供的日期中，如果所指定的星期几的值与
+     * 按照该日期中剩余部分所确定的星期几的值不符合，
+     * 那么该指定值就会被忽略。
+     * 例如，尽管 1996 年 11 月 9 日实际上是星期五，
+     * "Tuesday November 9 1996" 也还是可以被接受并进行解析的。
+     * 但是结果 date 对象中包含的是 "Friday November 9 1996"。
+     * - JScript 处理所有的标准时区，
+     * 以及全球标准时间 (UTC) 和格林威治标准时间 (GMT)。
+     * - 小时、分钟、和秒钟之间用冒号分隔，
+     * 尽管不是这三项都需要指明。"10:"、"10:11"、和 "10:11:12" 都是有效的。
+     * - 如果使用 24 小时计时的时钟，
+     * 那么为中午 12 点之后的时间指定"PM"是错误的。例如 "23:15 PM" 就是错误的。
+     * - 包含无效日期的字符串是错误的。
+     * 例如，一个包含有两个年份或两个月份的字符串就是错误的。
      *             
      * @returns {Date} 转换后的日期对象
      */
     lib.date.parse = function (source) {
         // by Tangram 1.x: baidu.date.parse
-        var reg = new RegExp("^\\d+(\\-|\\/)\\d+(\\-|\\/)\\d+\x24");
+        var reg = new RegExp('^\\d+(\\-|\\/)\\d+(\\-|\\/)\\d+\x24');
         if ('string' == typeof source) {
             if (reg.test(source) || isNaN(Date.parse(source))) {
-                var d = source.split(/ |T/),
-                    d1 = d.length > 1 
-                            ? d[1].split(/[^\d]/) 
-                            : [0, 0, 0],
-                    d0 = d[0].split(/[^\d]/);
-                return new Date(d0[0] - 0, 
-                                d0[1] - 1, 
-                                d0[2] - 0, 
-                                d1[0] - 0, 
-                                d1[1] - 0, 
-                                d1[2] - 0);
-            } else {
+                var d = source.split(/ |T/);
+                var d1 = d.length > 1 
+                    ? d[1].split(/[^\d]/) 
+                    : [0, 0, 0];
+                var d0 = d[0].split(/[^\d]/);
+                return new Date(
+                    d0[0] - 0, d0[1] - 1, d0[2] - 0, 
+                    d1[0] - 0, d1[1] - 0, d1[2] - 0
+                );
+            }
+            else {
                 return new Date(source);
             }
         }
@@ -887,16 +902,15 @@ define(function () {
      * @returns {string} 对目标数字进行0补齐处理后的结果
      */
     lib.number.pad = function (source, length) {
-        // by Tangram 1.x: baidu.number.pad
-        var pre = "",
-            negative = (source < 0),
-            string = String(Math.abs(source));
+        var pre = '';
+        var negative = (source < 0);
+        var string = String(Math.abs(source));
 
         if (string.length < length) {
             pre = (new Array(length - string.length + 1)).join('0');
         }
 
-        return (negative ?  "-" : "") + pre + string;
+        return (negative ?  '-' : '') + pre + string;
     };
 
     return lib;
