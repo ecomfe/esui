6c9826a42fe9c94041b96afeaee0c3d1c884fda9
otakustay <otakustay@gmail.com>
2013-05-08 20:21:37 +0800

    Added defaultExpand option to TreeStrategy

--- src/TreeStrategy.js
+++ src/TreeStrategy.js
@@ -1,16 +1,50 @@
 define(
     function (require) {
-        function TreeStrategy() {
+        var lib = require('./lib');
+
+        /**
+         * 树的数据交互策略
+         *
+         * @param {Object=} options 初始化参数
+         * @param {boolean=} options.defaultExpand 节点是否展开，默认为`false`
+         * @constructor
+         * @public
+         */
+        function TreeStrategy(options) {
+            var defaults = {
+                defaultExpand: false
+            };
+            lib.extend(this, defaults, options);
         }
 
+        /**
+         * 判断一个节点是否叶子节点
+         *
+         * @param {Object} node 节点数据项
+         * @return {boolean}
+         * @public
+         */
         TreeStrategy.prototype.isLeafNode = function (node) {
             return !node.children || !node.children.length;
         };
 
+        /**
+         * 判断一个节点是否应该展开
+         *
+         * @param {Object} node 节点数据项
+         * @return {boolean}
+         * @public
+         */
         TreeStrategy.prototype.shouldExpand = function (node) {
-            return false;
+            return this.defaultExpand;
         };
 
+        /**
+         * 将当前策略依附到控件上
+         *
+         * @param {Tree} tree 控件实例
+         * @public
+         */
         TreeStrategy.prototype.attachTo = function (tree) {
             tree.on(
                 'expand',
