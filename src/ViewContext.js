06a2eb34712e8342d40502f7e8efa1e088f1661c
erik <erik168@163.com>
2013-05-09 18:33:50 +0800

    add id generator and pool

--- src/ViewContext.js
+++ src/ViewContext.js
@@ -3,10 +3,30 @@
  * Copyright 2013 Baidu Inc. All rights reserved.
  * 
  * @file 视图环境类 用于对控件视图的管理
- * @author DBear
+ * @author DBear, errorrik
  */
 define(
     function () {
+        var counter = 0x830903;
+
+        /**
+         * 获取唯一id
+         * 
+         * @inner
+         * @return {string}
+         */
+        function getGUID() {
+            return 'vt' + counter++;
+        }
+
+        /**
+         * 视图环境对象池
+         * 
+         * @inner
+         * @type {Object}
+         */
+        var pool = {};
+
         /**
          * ViewContext类声明
          *
@@ -15,11 +35,31 @@ define(
         function ViewContext() {
             /**
              * 视图环境控件集合
+             * 
              * @type {Object} 
              */
             this.controls = {};
+
+            /**
+             * 视图环境id
+             * 
+             * @type {string} 
+             */
+            this.id = getGUID();
+
+            // 入池
+            pool[this.id] = this;
         }
 
+        /**
+         * 根据id获取视图环境
+         *
+         * @param {string} id 视图环境id
+         * @static
+         */
+        ViewContext.get = function ( id ) {
+            return pool[id] || null;
+        };
 
         /**
          * 将控件实例添加到视图环境中
@@ -66,14 +106,13 @@ define(
          * @public
          */
         ViewContext.prototype.get = function (id) {
-
             return this.controls[id];
-
         };
 
         /**
          * 清除视图环境中所有控件。 
          *
+         * @public
          */
         ViewContext.prototype.clean = function () {
             for (var id in this.controls) {
@@ -85,7 +124,16 @@ define(
             }
         };
 
-        return ViewContext;
+        /**
+         * 销毁视图环境 
+         *
+         * @public
+         */
+        ViewContext.prototype.dispose = function () {
+            this.clean();
+            delete pool[this.id];
+        };
 
+        return ViewContext;
     }
 );
