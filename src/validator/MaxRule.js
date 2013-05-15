01b5d8003f98b3e7580529d605ff82eea4c3926f
otakustay <otakustay@gmail.com>
2013-05-15 19:37:49 +0800

    Added common validation rules

---
+++ src/validator/MaxRule.js
@@ -0,0 +1,56 @@
+/**
+ * ESUI (Enterprise UI)
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file 字段最大长度验证规则
+ * @author otakustay
+ */
+define(
+    function (require) {
+        var Rule = require('./Rule');
+
+        /**
+         * MaxRule类声明
+         *
+         * @constructor
+         */
+        function MaxRule() {
+            Rule.apply(this, arguments);
+        }
+
+        /**
+         * 规则类型
+         * 
+         * @type {string}
+         */
+        MaxRule.prototype.type = 'max';
+
+
+        /**
+         * 错误提示信息
+         *
+         * @type {string}
+         */
+        MaxRule.prototype.errorMessage = 
+            '${title}不能大于${max}';
+
+        /**
+         * 验证控件的验证状态
+         *
+         * @param {string} value 校验值
+         * @param {Control} control 待校验控件
+         *
+         * @return {validator/ValidityState}
+         */
+        MaxRule.prototype.check = function (value, control) {
+            return new ValidityState(
+                !isNaN(value) && value <= this.getLimitCondition(), 
+                this.getErrorMessage(control)
+            );
+        };
+
+        require('./lib').inherits(MaxRule, Rule);
+        require('./main').registerRule(MaxRule);
+        return MaxRule;
+    }
+);
---
+++ src/validator/RequiredRule.js
@@ -0,0 +1,52 @@
+/**
+ * ESUI (Enterprise UI)
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file 必填项验证规则
+ * @author otakustay
+ */
+define(
+    function (require) {
+        var Rule = require('./Rule');
+
+        /**
+         * RequiredRule类声明
+         *
+         * @constructor
+         */
+        function RequiredRule() {
+            Rule.apply(this, arguments);
+        }
+
+        /**
+         * 规则类型
+         * 
+         * @type {string}
+         */
+        RequiredRule.prototype.type = 'required';
+
+
+        /**
+         * 错误提示信息
+         *
+         * @type {string}
+         */
+        RequiredRule.prototype.errorMessage = '${title}不能为空';
+
+        /**
+         * 验证控件的验证状态
+         *
+         * @param {string} value 校验值
+         * @param {Control} control 待校验控件
+         *
+         * @return {validator/ValidityState}
+         */
+        RequiredRule.prototype.check = function (value, control) {
+            return new ValidityState(!!value, this.getErrorMessage(control));
+        };
+
+        require('./lib').inherits(RequiredRule, Rule);
+        require('./main').registerRule(RequiredRule);
+        return RequiredRule;
+    }
+);
