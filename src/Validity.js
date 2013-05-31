09e1b4759c03c2a3ee3e60479114c64608d12dec
otakustay <otakustay@gmail.com>
2013-05-31 15:46:05 +0800

    Ehnahnced Validity control
    - Ignored state related class names when initializing
    - Ignored `for` attribute on label when focus target has no `id`
    attribute

--- src/Validity.js
+++ src/Validity.js
@@ -7,31 +7,59 @@ define(
         var helper = require('./controlHelper');
         var Control = require('./Control');
 
+        /**
+         * 验证信息显示控件
+         *
+         * @extends Control
+         * @constructor
+         */
         function Validity() {
             Control.apply(this, arguments);
         }
 
         Validity.prototype.type = 'Validity';
 
+        /**
+         * 创建主元素
+         *
+         * @param {HTMLElement} 肯定返回`<label>`
+         * @protected
+         */
         Validity.prototype.createMain = function () {
             return document.createElement('label');
         };
 
+        /**
+         * 初始化结构
+         *
+         * @protected
+         */
         Validity.prototype.initStructure = function () {
             if (this.main.nodeName.toLowerCase() === 'label') {
-                if (this.focusTarget) {
+                if (this.focusTarget && this.focusTarget.id) {
                     lib.setAttribute(this.main, 'for', this.focusTarget.id);
                 }
             }
         };
 
+        /**
+         * 获取元素的全部class
+         *
+         * @param {Validity} label 控件实例
+         * @param {string} state 验证状态
+         * @inner
+         */
         function getClasses(label, state) {
             var classes = [].concat(
                 helper.getPartClasses(label),
-                helper.getPartClasses(label.target, 'validity'),
-                helper.getPartClasses(label, state),
-                helper.getPartClasses(label.target, 'validity-' + state)
+                helper.getPartClasses(label.target, 'validity')
             );
+            if (state) {
+                classes = classes.concat(
+                    helper.getPartClasses(label, state),
+                    helper.getPartClasses(label.target, 'validity-' + state)
+                );
+            }
             if (label.target.isHidden() || label.isHidden()) {
                 classes = classes.concat(
                     helper.getStateClasses(label, 'hidden'),
@@ -41,6 +69,22 @@ define(
             return classes;
         }
 
+        /**
+         * 显示验证信息，可重写
+         *
+         * @param {string} validState 验证结果
+         * @param {string} message 验证信息
+         * @param {validator/Validity} 最原始的验证结果对象
+         *
+         * @protected
+         */
+        Validity.prototype.display = function (validState, message, validity) {
+            this.main.innerHTML = message;
+        };
+
+        /**
+         * 重绘
+         */
         Validity.prototype.repaint = helper.createRepaint(
             Control.prototype.repaint,
             {
@@ -63,7 +107,7 @@ define(
                                 }
                             }
                         }
-                        label.main.innerHTML = message || '';
+                        label.display(validState, message || '', validity);
                         label.initChildren();
                     }
                     else {
@@ -73,6 +117,9 @@ define(
             }
         );
 
+        /**
+         * 销毁控件
+         */
         Validity.prototype.dispose = function () {
             if (helper.isInStage(this, 'DISPOSED')) {
                 return;
