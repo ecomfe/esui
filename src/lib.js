3058370f811dbd59a8189390ad29f69a458e7a46
otakustay <otakustay@gmail.com>
2013-08-28 12:13:22 +0800

    Added space character to HTML entities

--- src/lib.js
+++ src/lib.js
@@ -259,7 +259,8 @@ define(function () {
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;')
             .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;');
+            .replace(/'/g, '&#39;')
+            .replace(/ /g, '&nbsp;');
     };
 
     /**
