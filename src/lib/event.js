1d1f56cb43d62fbcebda57a728da9d69fa95c7f0
otakustay <otakustay@gmail.com>
2013-11-16 23:22:11 +0800

    重构lib/event

--- src/lib/event.js
+++ src/lib/event.js
@@ -7,15 +7,17 @@
  */
 define(
     function (require) {
+        var dom = require('./dom');
         var event = {};
 
         /**
          * 阻止事件默认行为
          *
-         * @method module:event.preventDefault
-         * @param {Event} event 事件对象
+         * @param {Event} [event] 事件对象
          */
         event.preventDefault = function (event) {
+            event = event || window.event;
+
             if (event.preventDefault) {
                 event.preventDefault();
             }
@@ -27,10 +29,11 @@ define(
         /**
          * 阻止事件冒泡
          *
-         * @method module:event.stopPropagation
-         * @param {Event} event 事件对象
+         * @param {Event} [event] 事件对象
          */
         event.stopPropagation = function (event) {
+            event = event || window.event;
+            
             if (event.stopPropagation) {
                 event.stopPropagation();
             }
@@ -42,41 +45,46 @@ define(
         /**
          * 获取鼠标位置
          *
-         * @method module:event.getMousePosition
-         * @param {Event} event 事件对象
+         * @param {Event} [event] 事件对象
          */
         event.getMousePosition = function (event) {
+            event = event || window.event;
+            
             var doc = document.documentElement;
             var body = document.body;
 
-            event.pageX = event.pageX || (
-                event.clientX +
-                    (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-                    (doc && doc.clientLeft || body && body.clientLeft || 0)
-                );
+            // TODO: 用`page`模块
 
-            event.pageY = event.pageY || (
-                event.clientY +
-                    (doc && doc.scrollTop || body && body.scrollTop || 0) -
-                    (doc && doc.clientTop || body && body.clientTop || 0)
-                );
+            if (!event.pageX) {
+                event.pageX = event.clientX
+                    + (doc && doc.scrollLeft || body && body.scrollLeft || 0)
+                    - (doc && doc.clientLeft || body && body.clientLeft || 0);
+            }
+
+            if (!event.pageY) {
+                event.pageY = event.clientY
+                    + (doc && doc.scrollTop || body && body.scrollTop || 0)
+                    - (doc && doc.clientTop || body && body.clientTop || 0);
+            }
         };
 
         /**
          * 阻止事件冒泡
          *
-         * @method module:event.getTarget
-         * @param {Event} e 事件对象
+         * @param {Event} event 事件对象
          *
-         * @return {Object} 获取事件目标对象
+         * @return {HTMLElement} 事件目标对象
          */
-        event.getTarget = function (e) {
-            e = e || window.event;
-            return e.target || e.srcElement;
+        event.getTarget = function (event) {
+            event = event || window.event;
+
+            return event.target || event.srcElement;
         };
 
         return {
             on: function (element, type, listener) {
+                element = dom.g(element);
+                
                 if (element.addEventListener) {
                     element.addEventListener(type, listener, false);
                 }
@@ -86,6 +94,8 @@ define(
             },
 
             un: function (element, type, listener) {
+                element = dom.g(element);
+                
                 if (element.addEventListener) {
                     element.removeEventListener(type, listener, false);
                 }
