cbb7f86e73c9b34e66393b6a9d2199d8841987b8
otakustay <otakustay@gmail.com>
2013-11-21 01:31:26 +0800

    重构CheckBox
    - 去`controlHelper`化
    - 使用`underscore`

--- src/CheckBox.js
+++ src/CheckBox.js
@@ -8,19 +8,18 @@
 
 define(
     function (require) {
+        var u = require('underscore');
         var lib = require('./lib');
-        var helper = require('./controlHelper');
         var InputControl = require('./InputControl');
 
         /**
          * 同步选中状态
          *
-         * @param {CheckBox} box 控件实例
          * @param {Event} e DOM事件对象
          */
-        function syncChecked(box, e) {
-            var checked = lib.g(box.boxId).checked;
-            box.setProperties({ checked: checked });
+        function syncChecked(e) {
+            var checked = lib.g(this.boxId).checked;
+            this.setProperties({ checked: checked });
         }
 
         /**
@@ -60,7 +59,7 @@ define(
                     checked: false
                 };
 
-                lib.extend(properties, options);
+                u.extend(properties, options);
 
                 properties.name = 
                     properties.name || this.main.getAttribute('name');
@@ -80,13 +79,14 @@ define(
                 // 因此要先用一下，然后再管`datasource`
                 this.setProperties(properties);
                 if (datasource != null) {
-                    if (lib.isArray(datasource)) {
-                        for (var i = 0; i < datasource.length; i++) {
-                            if (datasource[i] == this.value) {
-                                this.checked = true;
-                                break;
-                            }
-                        }
+                    if (u.isArray(datasource)) {
+                        this.checked = u.any(
+                            datasource,
+                            function (item) {
+                                return item.value == this.value;
+                            },
+                            this
+                        );
                     }
                     else if (this.rawValue == datasource) {
                         this.checked = true;
@@ -95,7 +95,7 @@ define(
 
                 if (!this.title) {
                     this.title = this.main.title
-                        || ( this.getValue() === 'on' ? '' : this.getValue() );
+                        || (this.getValue() === 'on' ? '' : this.getValue());
                 }
             },
             
@@ -107,12 +107,12 @@ define(
             initStructure: function () {
                 // 如果用的是一个`<input>`，替换成`<div>`
                 if (this.main.nodeName.toLowerCase() === 'input') {
-                    this.boxId = this.main.id || helper.getId(this, 'box');
-                    helper.replaceMain(this);
+                    this.boxId = this.main.id || this.helper.getId('box');
+                    this.helper.replaceMain();
                     this.main.id = this.helper.getId();
                 }
                 else {
-                    this.boxId = helper.getId(this, 'box');
+                    this.boxId = this.helper.getId('box');
                 }
 
                 var html = '<input type="checkbox" name="${name}" id="${id}" />'
@@ -122,30 +122,24 @@ define(
                     {
                         name: this.name,
                         id: this.boxId,
-                        textId: helper.getId(this, 'text')
+                        textId: this.helper.getId('text')
                     }
                 );
 
-                var box = lib.g(helper.getId(this, 'box'));
-                helper.addDOMEvent(
-                    this, 
+                var box = lib.g(this.boxId);
+                this.helper.addDOMEvent(
                     box, 
                     'click', 
                     function (e) {
                         this.fire('click');
                         if (!box.addEventListener) {
-                            syncChecked(this, e);
+                            syncChecked.call(e);
                         }
                     }
                 );
 
                 if (box.addEventListener) {
-                    helper.addDOMEvent(
-                        this,
-                        box,
-                        'change',
-                        lib.curry(syncChecked, this)
-                    );
+                    this.helper.addDOMEvent(box, 'change', syncChecked);
                 }
             },
 
@@ -165,13 +159,13 @@ define(
             updateTitle: function (title) {
                 var title = this.title 
                     || this.main.title 
-                    || ( this.getValue() === 'on' ? '' : this.getValue());
-                title = lib.encodeHTML(title);
-                lib.g(helper.getId(this, 'text')).innerHTML = title;
+                    || (this.getValue() === 'on' ? '' : this.getValue());
+                title = u.escape(title);
+                this.helper.getPart('text').innerHTML = title;
                 lib.setAttribute(this.boxId, 'title', title);
             },
 
-            repaint: helper.createRepaint(
+            repaint: require('./painters').createRepaint(
                 InputControl.prototype.repaint,
                 {
                     name: ['rawValue', 'checked'],
@@ -215,8 +209,8 @@ define(
              * @return {boolean}
              */
             isChecked: function () {
-                if (helper.isInStage(this, 'RENDERED')) {
-                    var box = lib.g(helper.getId(this, 'box'));
+                if (this.helper.isInStage('RENDERED')) {
+                    var box = lib.g(this.boxId);
                     return box.checked;
                 }
                 else {
