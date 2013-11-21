2e0a5ac5ab39fe85137d923ab495f1bdd3e79e16
otakustay <otakustay@gmail.com>
2013-11-21 15:13:58 +0800

    重构Link控件

--- src/Link.js
+++ src/Link.js
@@ -5,12 +5,10 @@
  * @file Link控件
  * @author miaojian
  */
-
 define(
     function (require) {
-        var lib     = require('./lib');
+        var lib = require('./lib');
         var Control = require('./Control');
-        var helper  = require('./controlHelper');
         var paint = require('./painters');
 
         /**
@@ -22,12 +20,7 @@ define(
             Control.apply(this, arguments);
         }
 
-        function dispathClickEvent() {
-            this.fire('click');
-        }
-
         Link.prototype = {
-
             constructor: Link,
 
             /**
@@ -41,20 +34,20 @@ define(
              * 创建控件主元素
              *
              * @return {HTMLElement}
-             * @override
              * @protected
+             * @override
              */
             createMain: function () {
-                //主元素只能是a元素
+                // 主元素只能是a元素
                 return document.createElement('a');
             },
 
             /**
              * 初始化参数
              *
-             * @param {Object=} options 构造函数传入的参数
-             * @override
+             * @param {Object} [options] 构造函数传入的参数
              * @protected
+             * @override
              */
             initOptions: function (options) {
                 var properties = {};
@@ -84,9 +77,10 @@ define(
              * 初始化DOM结构
              *
              * @protected
+             * @override
              */
             initStructure: function () {
-                helper.addDOMEvent(this, this.main, 'click', dispathClickEvent);
+                this.helper.delegateDOMEvent(this.main, 'click');
             },
 
             /**
@@ -96,7 +90,6 @@ define(
                 // 如果控件主元素不为A,则不执行渲染操作
                 // 目前处理是让其一直处于init状态下
                 if (this.main && this.main.nodeName.toLowerCase() === 'a') {
-
                     Control.prototype.render.apply(this, arguments);
                 }
             },
@@ -104,20 +97,19 @@ define(
             /**
              * 渲染自身
              *
-             * @override
              * @protected
+             * @override
              */
-            repaint: helper.createRepaint(
+            repaint: paint.createRepaint(
                 Control.prototype.repaint,
                 paint.attribute('href'),
                 paint.attribute('target'),
-                paint.attribute('content'),
                 {
                     name: 'content',
-                    paint: function (control, value) {
-                        control.disposeChildren();
-                        control.main.innerHTML = value;
-                        control.initChildren(control.main);
+                    paint: function (link, content) {
+                        link.helper.disposeChildren();
+                        link.main.innerHTML = content;
+                        link.helper.initChildren();
                     }
                 }
             )
