6fa463054ca537c410a982f58c0fe8f02bbe4912
GU Yiling <justice360@gmail.com>
2014-05-24 10:48:48 +0800

    impl #289

--- src/BoxGroup.js
+++ src/BoxGroup.js
@@ -114,11 +114,19 @@ define(
             };
             u.extend(properties, options);
 
-            if (!properties.datasource.length) {
+            var datasource = properties.datasource;
+            if (!datasource.length) {
                 extractDatasourceFromDOM(this.main, properties);
             }
             if (!properties.rawValue && !properties.value) {
-                properties.rawValue = [];
+                // 单选框组在没有指定`value`时默认选中第一项
+                if (properties.boxType === 'radio'
+                    && datasource.length) {
+                    properties.rawValue = datasource[0].value;
+                }
+                else {
+                    properties.rawValue = [];
+                }
             }
 
             this.setProperties(properties);
