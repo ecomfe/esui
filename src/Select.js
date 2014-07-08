b93f4eee69d1b5f94efadb9c3a27c32b24591fd0
DDDBear <talent_256@yahoo.com.cn>
2014-07-08 17:50:10 +0800

    Select增加Layer的自定义class逻辑
    Select增加Layer的自定义class逻辑

--- src/Select.js
+++ src/Select.js
@@ -329,7 +329,14 @@ define(
          */
         Select.prototype.initEvents = function () {
             this.helper.addDOMEvent(this.main, 'click', u.bind(this.layer.toggle, this.layer));
+            this.layer.on('rendered', u.bind(addLayerClass, this));
         };
+
+        function addLayerClass() {
+            var layerClasses = this.helper.getPartClasses('layer');
+            this.layer.addCustomClasses(layerClasses);
+            this.fire('layerrendered', { layer: this.layer });
+        }
 
         /**
          * 根据控件的值更新其视图
