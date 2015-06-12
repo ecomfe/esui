553fe91f5160831f268f50420b9f2097c015c970
maoquan <maoquan@baidu.com>
2015-06-12 17:41:24 +0800

    代码整理 -- T

--- src/Tip.js
+++ src/Tip.js
@@ -8,9 +8,10 @@
  */
 define(
     function (require) {
+        var eoo = require('eoo');
+        var esui = require('./main');
         var u = require('underscore');
         var Control = require('./Control');
-        var ui = require('./main');
         var lib = require('./lib');
 
         require('./TipLayer');
@@ -24,79 +25,156 @@ define(
          * @requires TipLayer
          * @constructor
          */
-        function Tip(options) {
-            Control.apply(this, arguments);
-        }
-
-        /**
-         * 控件类型，始终为`"Tip"`
-         *
-         * @type {string}
-         * @readonly
-         * @override
-         */
-        Tip.prototype.type = 'Tip';
-
-        /**
-         * 初始化参数
-         *
-         * @param {Object} [options] 构造函数传入的参数
-         * @protected
-         * @override
-         */
-        Tip.prototype.initOptions = function (options) {
-            // 默认选项配置
-            var properties = {
-                title: '',
-                content: '',
-                /**
-                 * @property {boolean} arrow
-                 *
-                 * 是否需要箭头
-                 *
-                 * 为了方便从DOM生成，此属性在初始化时如果为字符串`"false"`，
-                 * 将被认为是布尔值`false`处理
-                 *
-                 * 具体参考{@link TipLayer#arrow}属性
-                 */
-                arrow: true,
+        var Tip = eoo.create(
+            Control,
+            {
 
                 /**
-                 * @property {string} showMode
+                 * 控件类型，始终为`"Tip"`
                  *
-                 * 指定信息浮层的显示方案，
-                 * 具体参考{@link TipLayer#attachTo}方法的`showMode`参数的说明
+                 * @type {string}
+                 * @readonly
+                 * @override
                  */
-                showMode: 'over',
+                type: 'Tip',
 
                 /**
-                 * @property {number} delayTime
+                 * 初始化参数
                  *
-                 * 指定信息浮层的显示的延迟时间，以毫秒为单位，
-                 * 具体参考{@link TipLayer#attachTo}方法的`delayTime`参数的说明
+                 * @param {Object} [options] 构造函数传入的参数
+                 * @protected
+                 * @override
                  */
-                delayTime: 500,
+                initOptions: function (options) {
+                    // 默认选项配置
+                    var properties = {
+                        title: '',
+                        content: '',
+                        /**
+                         * @property {boolean} arrow
+                         *
+                         * 是否需要箭头
+                         *
+                         * 为了方便从DOM生成，此属性在初始化时如果为字符串`"false"`，
+                         * 将被认为是布尔值`false`处理
+                         *
+                         * 具体参考{@link TipLayer#arrow}属性
+                         */
+                        arrow: true,
+
+                        /**
+                         * @property {string} showMode
+                         *
+                         * 指定信息浮层的显示方案，
+                         * 具体参考{@link TipLayer#attachTo}方法的`showMode`参数的说明
+                         */
+                        showMode: 'over',
+
+                        /**
+                         * @property {number} delayTime
+                         *
+                         * 指定信息浮层的显示的延迟时间，以毫秒为单位，
+                         * 具体参考{@link TipLayer#attachTo}方法的`delayTime`参数的说明
+                         */
+                        delayTime: 500,
+
+                        /**
+                         * @property {string} icon
+                         *
+                         * 用于输出内部icon
+                         */
+                        icon: 'question-circle'
+                    };
+
+                    u.extend(properties, options);
+                    if (options.arrow === 'false') {
+                        properties.arrow = false;
+                    }
+                    extractDOMProperties(this, properties);
+                    this.setProperties(properties);
+                },
 
                 /**
-                 * @property {string} icon
+                 * 初始化DOM结构
                  *
-                 * 用于输出内部icon
+                 * @protected
+                 * @override
                  */
-                icon: 'question-circle'
-            };
+                initStructure: function () {
+                    var main = document.createElement('div');
+                    document.body.appendChild(main);
+                    if (this.inheritFont || esui.getConfig('inheritFont')) {
+                        main.style.fontSize = lib.getComputedStyle(this.main, 'fontSize');
+                    }
+                    var tipLayer = esui.create(
+                        'TipLayer',
+                        {
+                            main: main,
+                            childName: 'layer',
+                            content: this.content,
+                            title: this.title,
+                            arrow: this.arrow,
+                            /**
+                             * @property {number} [layerWidth=200]
+                             *
+                             * 指定信息浮层的宽度，具体参考{@link TipLayer#width}属性
+                             */
+                            width: this.layerWidth || 200,
+                            viewContext: this.viewContext,
+                            // 添加一个类以方便区别inline tiplayer和全局tiplayer
+                            variants: 'from-tip'
+                        }
+                    );
+                    this.addChild(tipLayer);
+                    tipLayer.render();
 
-            u.extend(properties, options);
-            if (options.arrow === 'false') {
-                properties.arrow = false;
+                    var attachOptions = {
+                        // 原来老的代码写的是this.mode
+                        // 其实暴露的是showMode: 'over',
+                        // 这样向后兼容一下
+                        showMode: this.mode || this.showMode,
+                        delayTime: this.delayTime,
+                        targetControl: this.id,
+                        positionOpt: {top: 'top', right: 'left'}
+                    };
+                    tipLayer.attachTo(attachOptions);
+                },
+
+                /**
+                 * 重渲染
+                 *
+                 * @method
+                 * @protected
+                 * @override
+                 */
+                repaint: require('./painters').createRepaint(
+                    Control.prototype.repaint,
+                    {
+                        name: 'title',
+                        paint: function (tip, value) {
+                            var layer = tip.getChild('layer');
+                            if (layer) {
+                                layer.setTitle(value);
+                            }
+                        }
+                    },
+                    {
+                        name: 'content',
+                        paint: function (tip, value) {
+                            var layer = tip.getChild('layer');
+                            if (layer) {
+                                layer.setContent(value);
+                            }
+                        }
+                    }
+                )
             }
-            extractDOMProperties(this, properties);
-            this.setProperties(properties);
-        };
+        );
 
         /**
          * 从DOM中抽取`title`和`content`属性，如果有的话优先级低于外部给定的
          *
-         * @param {HTMLElement} 主元素
+         * @param {HTMLElement} tip 主元素
          * @param  {Object} options 构造函数传入的参数
          * @ignore
          */
@@ -114,83 +192,7 @@ define(
             main.innerHTML = html;
         }
 
-        /**
-         * 初始化DOM结构
-         *
-         * @protected
-         * @override
-         */
-        Tip.prototype.initStructure = function () {
-            var main = document.createElement('div');
-            document.body.appendChild(main);
-            if (this.inheritFont || ui.getConfig('inheritFont')) {
-                main.style.fontSize = lib.getComputedStyle(this.main, 'fontSize');
-            }
-            var tipLayer = ui.create(
-                'TipLayer',
-                {
-                    main: main,
-                    childName: 'layer',
-                    content: this.content,
-                    title: this.title,
-                    arrow: this.arrow,
-                    /**
-                     * @property {number} [layerWidth=200]
-                     *
-                     * 指定信息浮层的宽度，具体参考{@link TipLayer#width}属性
-                     */
-                    width: this.layerWidth || 200,
-                    viewContext: this.viewContext,
-                    // 添加一个类以方便区别inline tiplayer和全局tiplayer
-                    variants: 'from-tip'
-                }
-            );
-            this.addChild(tipLayer);
-            tipLayer.render();
-
-            var attachOptions = {
-                // 原来老的代码写的是this.mode
-                // 其实暴露的是showMode: 'over',
-                // 这样向后兼容一下
-                showMode: this.mode || this.showMode,
-                delayTime: this.delayTime,
-                targetControl: this.id,
-                positionOpt: {top: 'top', right: 'left'}
-            };
-            tipLayer.attachTo(attachOptions);
-        };
-
-        /**
-         * 重渲染
-         *
-         * @method
-         * @protected
-         * @override
-         */
-        Tip.prototype.repaint = require('./painters').createRepaint(
-            Control.prototype.repaint,
-            {
-                name: 'title',
-                paint: function (tip, value) {
-                    var layer = tip.getChild('layer');
-                    if (layer) {
-                        layer.setTitle(value);
-                    }
-                }
-            },
-            {
-                name: 'content',
-                paint: function (tip, value) {
-                    var layer = tip.getChild('layer');
-                    if (layer) {
-                        layer.setContent(value);
-                    }
-                }
-            }
-        );
-
-        lib.inherits(Tip, Control);
-        ui.register(Tip);
+        esui.register(Tip);
         return Tip;
     }
 );
