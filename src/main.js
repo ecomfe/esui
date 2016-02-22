805117d5bd1899ded89d4ec4acee790591d71d75
otakustay <otakustay@gmail.com>
2016-02-22 15:20:50 +0800

    更新版本至3.1.1

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ define(
          * @type {string}
          * @readonly
          */
-        main.version = '3.1.0';
+        main.version = '3.1.1';
 
         var ViewContext = require('./ViewContext');
         var defaultViewContext = new ViewContext('default');
