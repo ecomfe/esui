e40f6448e0f4cea0d2104f3c749b7d46eefce90a
xutingting02 <xutingting02@baidu.com>
2016-07-10 10:31:20 +0800

    config

--- demo/config.js
+++ demo/config.js
@@ -18,7 +18,7 @@ require.config({
         },
         {
             'name': 'etpl',
-            'location': '../dep/etpl/3.1.1/src',
+            'location': '../dep/etpl/3.2.0/src',
             'main': 'main'
         },
         {
