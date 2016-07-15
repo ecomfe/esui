db582f460676dd2336e16d8801135431b5512998
otakustay <otakustay@gmail.com>
2016-07-15 14:26:36 +0800

    修复initConnectedChildren的BUG

--- src/helper/children.js
+++ src/helper/children.js
@@ -22,11 +22,31 @@ define(
          * @param {HTMLElement} [wrap] 容器DOM元素，默认为主元素
          * @param {Object} [options] init参数
          * @param {Object} [options.properties] 属性集合，通过id映射
+         * @param {boolean} [connected = false] 生成的控件是否由当前控件作为owner
          * @return {Array} 子控件数组
          */
-        helper.initChildren = function (wrap, options) {
+        helper.initChildren = function (wrap, options, connected) {
             wrap = wrap || this.control.main;
-            options = u.extend({owner: this.control.owner}, this.control.renderOptions, options);
+
+            var control = this.control;
+            var rawValueReplacer = control.renderOptions && control.renderOptions.valueReplacer;
+            var source = connected ? control : control.owner;
+            var valueReplacer = function (value) {
+                if (value.charAt(0) === '@') {
+                    var path = value.substring(1).split('.');
+                    var propertyName = path.shift();
+                    var result = u.reduce(
+                        path,
+                        function (value, property) {
+                            return value[property];
+                        },
+                        source.get(propertyName)
+                    );
+                    return result;
+                }
+                return rawValueReplacer ? rawValueReplacer(value) : value;
+            };
+            options = u.extend({valueReplacer: valueReplacer, owner: control}, options);
             options.viewContext = this.control.viewContext;
             options.parent = this.control;
 
@@ -47,26 +67,7 @@ define(
          * @return {Array} 子控件数组
          */
         helper.initConnectedChildren = function (wrap, options) {
-            var control = this.control;
-            var rawValueReplacer = control.renderOptions && control.renderOptions.valueReplacer;
-            var source = control.owner || control;
-            var valueReplacer = function (value) {
-                if (value.charAt(0) === '@') {
-                    var path = value.substring(1).split('.');
-                    var propertyName = path.shift();
-                    var result = u.reduce(
-                        path,
-                        function (value, property) {
-                            return value[property];
-                        },
-                        source.get(propertyName)
-                    );
-                    return result;
-                }
-                return rawValueReplacer ? rawValueReplacer(value) : value;
-            };
-            options = u.extend({valueReplacer: valueReplacer, owner: control}, options);
-            return this.initChildren(wrap, options);
+            return this.initChildren(wrap, options, true);
         };
 
         /**
