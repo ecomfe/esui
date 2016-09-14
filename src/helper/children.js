35f39521ed66f58a12d413fa09f5e4b719c31e59
weifeng <weifeng@baidu.com>
2016-09-14 18:48:36 +0800

    revert code

--- src/helper/children.js
+++ src/helper/children.js
@@ -46,7 +46,7 @@ define(
                 }
                 return rawValueReplacer ? rawValueReplacer(value) : value;
             };
-            options = u.extend({valueReplacer: source ? valueReplacer : rawValueReplacer, owner: source}, options);
+            options = u.extend({valueReplacer: valueReplacer: rawValueReplacer, owner: control}, options);
             options.viewContext = this.control.viewContext;
             options.parent = this.control;
 
